2548c8f5e853d8be95e5f8c9c51c6990079b97fb
Yvan Yang <zjlgdx@gmail.com>
2014-04-29 22:28:37 +0800

    asp.net mvc 5 with bootstrap modal and client javascript validation

---
+++ PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using PartyInvite.Models;
+using System;
+using System.Web.Mvc;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+
+        }
+
+	}
+}
